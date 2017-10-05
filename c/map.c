c30ac0a9257edcebc3c57d3c42b55dbf48ad5015
raymonshansen <raymon.s.hansen@gmail.com>
2017-10-05 12:41:50 +0200

    Adds: Working on map data-type

--- c/map.c
+++ c/map.c
@@ -1,11 +1,32 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "map.h"
 
-int main(int argc, char **argv){
-    int width = 16;
-    int height = 4;
-    int y, x;
+struct map {
+    // Basic size (expand later...)
+    int width;
+    int height;
+    int wpad;
+    int hpad;
+
+
+}
+/* Creates a new map with given with and padding.
+*/
+map_t * map_create(int with, int height, int wpad, int hpad){
     // Allocate memory for the map
+    map_t *newmap = (map_t*)malloc(sizeof(map_t));
+    
+    if (NULL == newmap){
+        printf("Error in allocating space for map.");
+        return 0;
+    }
+    // Set the given size before making space
+    newmap->width = width;
+    newmap->height = height;
+    newmap->wpad = wpad;
+    newmap->hpad = hpad;
+    // The basic data-structure for the tiles is an array
     int **map = (int**)malloc(height * sizeof(int*));
     for(y = 0; y < height; y++){
         map[y] = (int*)malloc(width * sizeof(int));
--- c/map.h
+++ c/map.h
@@ -5,16 +5,23 @@
     with a lot of support functions. The map
     will likely be a huge struct containing 
     flags and information about itself.
-    Monsters??
-    Probably an array of tiles.
+    
+    Set up as an array of tiles.
+
 
     It will have a lot of support functions enabling
     the interaction with the map done in the game engine.
 
-    Listing some of them:
-    map_t *map_create();
-    
-
+    Map interface:
 */
+struct map;
+typedef struct map map_t;
+
+/*  map_create()
+    Creates an empty map with which 
+    the dungeon module can use to construct
+    a random dungeon.
+*/
+    map_t *map_create(int with, int height, int wpad, int hpad);
 
 #endif /* MAP_H_ */
