c510a0c3e5a4d98c6b7bce87bad66b02a50042ca
raymonshansen <raymon.s.hansen@gmail.com>
2018-06-10 09:19:55 +0200

    Delete font-files and move all into main python folder.

--- python/typebank.py
+++ python/typebank.py
@@ -10,7 +10,7 @@ class TypeBank():
 
     def load_all(self):
         for key in self.filenames:
-            path = os.path.join('tiles2', self.filenames.get(key, 'none.png'))
+            path = os.path.join('tiles', self.filenames.get(key, 'none.png'))
             img = pygame.image.load(path).convert_alpha()
             self.images.append(img)
 
